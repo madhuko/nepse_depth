fc098cbb7a17b78873d2e624eb10586358ea1242
madhuko <65185078+madhuko@users.noreply.github.com>
2021-07-24 11:35:45 +0545

    Update and rename meroshare.py to depth.py

---
+++ depth.py
@@ -0,0 +1,27 @@
+import requests,datetime
+
+header={
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36 Edg/90.0.818.66',
+    'Accept': '*/*',
+    'content-type': 'application/json',
+    'Accept-Encoding': 'gzip, deflate, br',
+}
+
+payload_id=requests.get("http://nepse-payload-id.samrid.me",headers=header)
+
+b_url="https://newweb.nepalstock.com.np/api/nots/security?nonDelisted=true"
+m_url="https://newweb.nepalstock.com.np/api/nots/nepse-data/marketdepth/{}"
+t_url="https://newweb.nepalstock.com.np/api/nots/security-detail/{}"
+
+basic_list=requests.get(b_url,headers=header).json()
+
+f= open("depth.txt","a")
+for i in basic_list[:]:
+  print(t_url.format(i["id"]))
+  resp=requests.get(t_url.format(i["id"]),headers=header).json()
+  text=str(datetime.datetime.now())
+  f.write(text)
+  f.write("\n")
+  f.write(str(resp))
+  f.write("\n")
+f.close()
