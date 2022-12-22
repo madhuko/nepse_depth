417cc17bd87b0ee2b39ca0555f6bfaadb0e61dd7
madhuko <65185078+madhuko@users.noreply.github.com>
2022-12-22 22:19:28 +0545

    Update depth_v2.py

--- depth_v2.py
+++ depth_v2.py
@@ -105,31 +105,31 @@ class Nepse:
     def __init__(self):
         self.token_parser     = TokenParser()
         
-        self.token_url            = "https://newweb.nepalstock.com/api/authenticate/prove"
+        self.token_url            = "https://www.nepalstock.com/api/authenticate/prove"
         
         self.price_volume_url     = "https://www.nepalstock.com.np/api/nots/securityDailyTradeStat/58"
-        self.summary_url          = "https://newweb.nepalstock.com.np/api/nots/market-summary/"
-        self.top_ten_scrips_url   = "https://newweb.nepalstock.com.np/api/nots/top-ten/trade-qty"
-        self.supply_demand_url    = "https://newweb.nepalstock.com.np/api/nots/nepse-data/supplydemand"
-        self.turnover_url         = "https://newweb.nepalstock.com.np/api/nots/top-ten/turnover"
-        self.top_gainers_url      = "https://newweb.nepalstock.com.np/api/nots/top-ten/top-gainer"
-        self.top_losers_url       = "https://newweb.nepalstock.com.np/api/nots/top-ten/top-loser"
-        self.nepse_open_url       = "https://newweb.nepalstock.com.np/api/nots/nepse-data/market-open"
-        self.nepse_index_url      = "https://newweb.nepalstock.com.np/api/nots/nepse-index"
-        self.nepse_subindices_url = "https://newweb.nepalstock.com/api/nots"
-        self.getStockDepth_url    ="https://newweb.nepalstock.com.np/api/nots/nepse-data/marketdepth/{}"
-        self.getListedStock_url="https://newweb.nepalstock.com.np/api/nots/securityDailyTradeStat/{}"
-        self.get_all_listed_url="https://newweb.nepalstock.com.np/api/nots/company/list"
-        self.get_market_depth_url="https://newweb.nepalstock.com.np/api/nots/nepse-data/marketdepth/{}"
+        self.summary_url          = "https://www.nepalstock.com.np/api/nots/market-summary/"
+        self.top_ten_scrips_url   = "https://www.nepalstock.com.np/api/nots/top-ten/trade-qty"
+        self.supply_demand_url    = "https://www.nepalstock.com.np/api/nots/nepse-data/supplydemand"
+        self.turnover_url         = "https://www.nepalstock.com.np/api/nots/top-ten/turnover"
+        self.top_gainers_url      = "https://www.nepalstock.com.np/api/nots/top-ten/top-gainer"
+        self.top_losers_url       = "https://www.nepalstock.com.np/api/nots/top-ten/top-loser"
+        self.nepse_open_url       = "https://www.nepalstock.com.np/api/nots/nepse-data/market-open"
+        self.nepse_index_url      = "https://www.nepalstock.com.np/api/nots/nepse-index"
+        self.nepse_subindices_url = "https://www.nepalstock.com.np/api/nots"
+        self.getStockDepth_url    ="https://www.nepalstock.com.np/api/nots/nepse-data/marketdepth/{}"
+        self.getListedStock_url="https://www.nepalstock.com.np/api/nots/securityDailyTradeStat/{}"
+        self.get_all_listed_url="https://www.nepalstock.com.np/api/nots/company/list"
+        self.get_market_depth_url="https://www.nepalstock.com.np/api/nots/nepse-data/marketdepth/{}"
 
         self.headers= {
-                            'Host': 'newweb.nepalstock.com',
+                            'Host': 'www.nepalstock.com.np',
                             'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0',
                             'Accept': 'application/json, text/plain, */*',
                             'Accept-Language': 'en-US,en;q=0.5',
                             'Accept-Encoding': 'gzip, deflate, br',
                             'Connection': 'keep-alive',
-                            'Referer': 'https://newweb.nepalstock.com/',
+                            'Referer': 'https://www.nepalstock.com/',
                             'Pragma': 'no-cache',
                             'Cache-Control': 'no-cache',
                             'TE': 'Trailers',
