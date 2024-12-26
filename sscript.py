e9f831bed9e02f1dada807eba6de880ca02edef3
madhuko <65185078+madhuko@users.noreply.github.com>
2024-12-26 19:25:37 +0545

    update 2

--- sscript.py
+++ sscript.py
@@ -2,7 +2,7 @@
 from nepse import AsyncNepse 
 import asyncio
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
 
 # %%
@@ -48,7 +48,7 @@ async def main():
     os.makedirs("files", exist_ok=True)
 
     # Generate filename with timestamp
-    now = datetime.now()- pd.Timedelta(hours=5, minutes=45)
+    now = datetime.now() - timedelta(hours=5, minutes=45)
     filename = now.strftime("%Y-%m-%d_%H-%M-%S")
     with open(f"files/{filename}.json", "w") as f:
         json.dump(results, f, indent=4)
